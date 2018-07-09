534e0b2723cf3b569e553987c5f8c733b597b7ba
Benjamin Simonson <bfsimon@gmail.com>
2018-07-09 16:44:41 -0400

    Sets up event listener for cards

--- js/app.js
+++ js/app.js
@@ -36,3 +36,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const deck = document.querySelector('.deck');
+
+deck.addEventListener('click', event => {
+  if (event.target.classList.contains('card')) {
+    console.log("You clicked a card!");
+  }
+});
