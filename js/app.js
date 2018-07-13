9e74d61bb40ea3ce78b973a725dc58e0e16f01c4
Benjamin Simonson <bfsimon@gmail.com>
2018-07-13 11:40:06 -0400

    Increases the number of moves after every turn of play

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 
 
 /*
- * Display the cards on the page
+ * visibility the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
@@ -28,16 +28,24 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - visibility the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + increment the move counter and visibility it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, visibility a message with the final score (put this functionality in another function that you call from this one)
  */
 
 const deck = document.querySelector('.deck');
+
+let moves = 0;
+
+function addMove() {
+  moves++;
+  const movesText = document.querySelector('.moves');
+  movesText.innerHTML = moves;
+}
 
 function shuffleDeck() {
   const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
@@ -55,6 +63,7 @@ deck.addEventListener('click', event => {
     addFlippedCard(card);
     if (flippedCards.length === 2) {
       checkMatch();
+      addMove();
     }
   }
 });
